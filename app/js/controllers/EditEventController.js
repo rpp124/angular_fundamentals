b7c00d1da816312226ef014aae1ef38775182454
Rick P <rpp124@yahoo.com>
2017-06-20 21:15:17 -0400

    asdfasdf

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,5 +1,10 @@
 'use strict';
 
 eventsApp.controller('EditEventController', function EditEventController($scope) {
-    
+    $scope.saveEvent = function(event) {
+    }
+
+    $scope.cancelEdit = function() {
+        window.location.href="/eventDetails.html";
+    }
 })
